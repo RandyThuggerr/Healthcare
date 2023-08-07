8f63b1942fa9b1d0dbf0bde7f75b2f770b2c6651
Victor <104492741+RandyThuggerr@users.noreply.github.com>
2023-08-07 02:40:03 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===========================typing animation ===========================*/
+var typed= new Typed(".typing",{
+    strings:[" ","Front-End Developer","UI/UX Designer","Software Engineer","Cyber Security Analyst","Game Dev"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}) 
